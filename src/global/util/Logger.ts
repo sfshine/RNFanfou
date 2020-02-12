8e21c3ef65844fdb8512157554d75fd3df52b94c
gaoshuai09 <gaoshuai09@meituan.com>
2020-02-12 13:33:50 +0800

    dev下才打印日志

--- src/global/util/Logger.ts
+++ src/global/util/Logger.ts
@@ -1,21 +1,26 @@
 import {formatDate} from "./DateUtil";
 
+const dev = __DEV__
 export default class Logger {
     static logPrefix: string = ""
 
     static log(tag, msg, ...optionalParams: any[]) {
+        if (!dev) return
         console.log(`${formatDate()} ${this.logPrefix}-${tag}:${msg}`, ...optionalParams)
     }
 
     static info(tag, msg, ...optionalParams: any[]) {
+        if (!dev) return
         console.info(`${formatDate()} ${this.logPrefix}-${tag}:${msg}`, ...optionalParams)
     }
 
     static warn(tag, msg, ...optionalParams: any[]) {
+        if (!dev) return
         console.warn(`${formatDate()} ${this.logPrefix}-${tag}:${msg}`, ...optionalParams)
     }
 
     static error(tag, msg, ...optionalParams: any[]) {
+        if (!dev) return
         console.warn(`${formatDate()} ${this.logPrefix}-${tag}:${msg}`, ...optionalParams)
     }
 }
