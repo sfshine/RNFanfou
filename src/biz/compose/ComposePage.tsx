fc1c0d2269a641b11b22cf1a667c8cb33b0e25bd
gaoshuai09 <gaoshuai09@meituan.com>
2020-02-22 22:56:10 +0800

    编辑界面折行显示

--- src/biz/compose/ComposePage.tsx
+++ src/biz/compose/ComposePage.tsx
@@ -39,6 +39,7 @@ class ComposePage extends React.PureComponent<Props, State> {
         let textInput = <TextInput
             onSelectionChange={(event) => this.selection = event.nativeEvent.selection}
             autoFocus={true}
+            multiline={true}
             placeholder={this.placeHolder}
             value={this.state.inputString}
             onChangeText={text => this.setState({inputString: text})}
